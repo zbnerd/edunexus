06b1b344edaf66e9b5a775e1cbcf514e7f9d7a46
Seung Jun Lee <mps86@naver.com>
2024-12-04 00:55:19 +0900

    feat: MSA - course service api test case

---
+++ edu-nexus-course-service/src/test/java/com/edunexuscourseservice/EduNexusCourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunexuscourseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNexusCourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
