ff27a0d7031b67f4378061c0cf02629c7bf567e1
Seung Jun Lee <mps86@naver.com>
2024-12-04 01:51:24 +0900

    feat: MSA - user service init

---
+++ edu-nexus-user-service/src/main/java/com/edunexususerservice/EduNexusUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edunexususerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNexusUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduNexusUserServiceApplication.class, args);
+	}
+
+}
---
+++ edu-nexus-user-service/src/test/java/com/edunexususerservice/EduNexusUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunexususerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNexusUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
