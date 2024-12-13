423ee666806489e38038010d2618acda3514bc97
Seung Jun Lee <mps86@naver.com>
2024-12-14 08:09:36 +0900

    feat: 파일매니징 서비스 초기화

---
+++ edu-nexus-file-manage-service/src/main/java/com/edunexusfilemanageservice/EduNexusFileManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edunexusfilemanageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNexusFileManageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduNexusFileManageServiceApplication.class, args);
+	}
+
+}
---
+++ edu-nexus-file-manage-service/src/test/java/com/edunexusfilemanageservice/EduNexusFileManageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunexusfilemanageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNexusFileManageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
