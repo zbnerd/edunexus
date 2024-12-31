2ff673f48b09c79c8748ce25930ceb3207207037
Seung Jun Lee <mps86@naver.com>
2024-12-31 10:48:30 +0900

    init: coupon service 초기화

---
+++ edu-nexus-coupon-service/src/main/java/com/edunexuscouponservice/EduNexusCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edunexuscouponservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNexusCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduNexusCouponServiceApplication.class, args);
+	}
+
+}
---
+++ edu-nexus-coupon-service/src/test/java/com/edunexuscouponservice/EduNexusCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunexuscouponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNexusCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
