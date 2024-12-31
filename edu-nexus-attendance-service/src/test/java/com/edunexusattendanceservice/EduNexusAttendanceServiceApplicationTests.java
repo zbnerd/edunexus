4a9cafc8b4d782a4832499e06cf47811a04dd7cc
Seung Jun Lee <mps86@naver.com>
2024-12-31 11:11:30 +0900

    init: attendance service 초기화

---
+++ edu-nexus-attendance-service/src/test/java/com/edunexusattendanceservice/EduNexusAttendanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edunexusattendanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNexusAttendanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
