7a11ad2ec5c5224b2666b19183bbfb389624dbff
Seung Jun Lee <mps86@naver.com>
2025-01-04 19:58:03 +0900

    feat: CourseRatingDeleteResponseEvent

--- edu-nexus-course-service/src/main/java/com/edunexuscourseservice/application/saga/event/CourseRatingDeleteResponseEvent.java
+++ edu-nexus-course-service/src/main/java/com/edunexuscourseservice/application/saga/event/CourseRatingDeleteResponseEvent.java
@@ -1,4 +1,15 @@
 package com.edunexuscourseservice.application.saga.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseRatingDeleteResponseEvent {
+    private String response; // 성공 또는 실패 상태 ("success", "fail")
+    private String method;   // 호출된 메서드명 ("CourseRatingDeleteEvent")
+    private Long courseId;   // 코스 ID
+    private int oldRating;   // 이전 평점
 }
